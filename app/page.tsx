dec78cff33f4dc5976d9e46049ef5d9373ca2266
mbiville-hash <282668003+mbiville-hash@users.noreply.github.com>
2026-06-25 22:38:13 +0200

    nnnrevert: retire le comparateur avant/apres de l'accueil (en attente de meilleures photos)

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import AvisC from '@/components/AvisC'
 import ZoneA from '@/components/ZoneA'
 import FormA from '@/components/FormA'
 import Realisations from '@/components/Realisations'
-import BeforeAfter from '@/components/BeforeAfter'
 import { BASE_SCHEMA } from '@/lib/schema'
 
 export const metadata: Metadata = {
@@ -36,7 +35,6 @@ export default function HomePage() {
         <ProsA />
         <PartB />
         <Realisations />
-        <BeforeAfter />
         <AvisC />
         <ZoneA />
         <FormA />
